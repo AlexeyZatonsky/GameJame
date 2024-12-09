533ab77c8aa65b0e5036bd8a22fd80c2063c3ec8
SharKPL <gameFlash464@gmail.com>
2024-12-09 22:21:04 +0300

    Manager0.01

--- GameJame/Assets/MyAssets/Scripts/Managers/TimeManager.cs
+++ GameJame/Assets/MyAssets/Scripts/Managers/TimeManager.cs
@@ -6,6 +6,9 @@ public class TimeManager : SingletonManager<TimeManager>
     //UI
     [SerializeField] private TextMesh timerText;
 
+    //
+    [SerializeField] private float timerAmount = 10;
+
     //Player
 
     //EVents
@@ -13,15 +16,32 @@ public class TimeManager : SingletonManager<TimeManager>
     public Action OnTimerStop;
 
 
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        OnTimerStart?.Invoke();
+        OnTimerStop += TestLog;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Time.time <= timerAmount)
+        {
+            Debug.Log(Time.time);
+            return;
+        }
+        else { 
+            OnTimerStop?.Invoke();
+        }  
         
+        
+    }
+
+    public void TestLog()
+    {
+        Debug.Log("TimerStopped");
     }
 }
