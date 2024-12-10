cb6224dc175197d007cabc0f93c7f94b79719a7d
Alexey Zatonsky <67155220+AlexeyZatonsky@users.noreply.github.com>
2024-12-10 19:04:05 +0300

    Todoшки

--- GameJame/Assets/MyAssets/Scripts/Interactive/InteractiveObject.cs
+++ GameJame/Assets/MyAssets/Scripts/Interactive/InteractiveObject.cs
@@ -1,13 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InteractiveObject : MonoBehaviour
 {
     [SerializeField] private IntractiveObjectData intractiveObjectData;
+    [SerializeField] private List<LootData> lootDatasList = new List<LootData>();
 
-
+    private bool canInteract = false;
 
     public void Interact()
     {
+        foreach (LootData toInteract in lootDatasList)
+        {
+            //TODO: проверяем совпадает ли лут в руках с одним из лутов для взаимодействия с объектом
+        }
+        
+        //TODO: запускаем анимацию
+        
+        
         
     }
 }
