c59b5e97230f0cc0eda6219c78c3e3c498de2488
SharKPL <gameFlash464@gmail.com>
2024-12-11 23:03:01 +0300

    CutSceneSceneTest

---
+++ GameJame/Assets/MyAssets/Scripts/Managers/CutsceneManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class CutsceneManager : SingletonManager<CutsceneManager>
+{
+    [Header("Video Settings")]
+    [SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private string nextSceneName;
+
+    private void Start()
+    {
+        if (videoPlayer == null)
+        {
+            Debug.LogError("No VideoPlayer.");
+            return;
+        }
+
+        videoPlayer.loopPointReached += OnVideoFinished; 
+        videoPlayer.Play();
+    }
+
+    private void OnVideoFinished(VideoPlayer vp)
+    {
+        Debug.Log("CutScene Completed.NewScene load...");
+        LoadNextSceneAsync();
+    }
+
+    private void LoadNextSceneAsync()
+    {
+        if (!string.IsNullOrEmpty(nextSceneName))
+        {
+            SceneManager.LoadSceneAsync(nextSceneName);
+        }
+        else
+        {
+            Debug.LogError("No Name for next Scene");
+        }
+    }
+}
